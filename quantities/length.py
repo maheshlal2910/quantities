e12cb2132e7b353adf89b2059901d72477e43c15
Mahesh Lal <mahesh.2910@gmail.com>
2015-08-18 12:09:27 +0530

    added conversion from millimeters to other unit using conversion factor

--- quantities/length.py
+++ quantities/length.py
@@ -1,2 +1,5 @@
 def to_millimeters(value, conversion_factor):
     return value * conversion_factor
+
+def from_millimeters(value, conversion_factor):
+    return 10
--- quantities/tests/length_test.py
+++ quantities/tests/length_test.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 from quantities.length import to_millimeters
+from quantities.length import from_millimeters
 
 
 class ToMillimetersTest(TestCase):
 
     def test_should_convert_to_millimeters_based_on_factor(self):
         assert to_millimeters(8, 10) == 80
+
+
+class FromMillimetersTest(TestCase):
+
+    def test_should_convert_from_100_millimeters_given_conversion_factor_of_10(self):
+        assert from_millimeters(100, 10.0) == 10.0
