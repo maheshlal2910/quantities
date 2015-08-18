4c1e76262e90b9dc765bc208ab933068b77b69b5
Mahesh Lal <mahesh.2910@gmail.com>
2015-08-18 11:37:11 +0530

    Correcting stupid placement of test

--- quantities/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from quantities.length import to_millimeters
-
-
-class ToMillimetersTest(TestCase):
-
-    def test_should_convert_to_millimeters_based_on_factor(self):
-        assert to_millimeters(8, 10) == 80
